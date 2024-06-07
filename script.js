3b78ce39017839933c882de6b5148e34ac9c2095
runelahmed <runelahmed71@gmail.com>
2024-06-07 10:57:19 +0300

    added login and forgot password form

---
+++ script.js
@@ -0,0 +1,10 @@
+const myAccount = document.querySelector('.userhub');
+
+const modal = document.getElementById('modal');
+
+
+myAccount.addEventListener('click', () => {
+
+    modal.classList.add('show-modal');
+
+});
